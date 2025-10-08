4caed1fd8f0550fc1edd98b73df75b847c2439ed
Luke Lee <leeluke0628@gmail.com>
2025-10-07 17:09:40 -0700

    FAQ first question changed

--- app/components/FAQ/index.tsx
+++ app/components/FAQ/index.tsx
@@ -13,14 +13,14 @@ const FAQ = () => {
                         {({ open }) => (
                             <>
                                 <Disclosure.Button className="flex w-full justify-between rounded-lg px-4 py-2 text-left text-2xl font-medium">
-                                    <span>Can you design my site?</span>
+                                    <span>How does Renvestment Work?</span>
                                     <ChevronUpIcon
                                         className={`${open ? 'rotate-180 transform' : ''
                                             } h-5 w-5 text-purple-500`}
                                     />
                                 </Disclosure.Button>
                                 <Disclosure.Panel className="px-4 pt-4 pb-2 text-base text-black font-normal opacity-50">
-                                    Craven omni memoria patriae zombieland clairvius narcisse <br /> religionis
+                                    Renvestment lets tenants pay rent online while earning cashback. When you pay rent through Renvestment, the money is automatically split <br /> religionis
                                     sunt diri undead historiarum. Golums, zombies unrelenting <br /> et Raimi fascinati beheading.
                                 </Disclosure.Panel>
                             </>
