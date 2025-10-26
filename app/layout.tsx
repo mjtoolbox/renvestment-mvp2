a5fc735db04aafa57ecbd25c2ac469b1648209b8
mjtoolbox <michael.jo@gmail.com>
2025-10-26 11:24:07 -0700

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/index';
-
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata = {
   title: 'Renvestment',
@@ -22,6 +22,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
