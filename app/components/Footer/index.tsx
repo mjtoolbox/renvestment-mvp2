6b9ac7772fb89b281bb9be51566767b374ba2ecb
mjtoolbox <michael.jo@gmail.com>
2025-10-26 10:25:20 -0700

    updated

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -44,13 +44,19 @@ const footer = () => {
                         <h3 className='text-white text-4xl font-semibold leading-9 mb-4 lg:mb-20'> Renvestment</h3>
                         <div className='flex gap-4'>
                             <div className='footer-icons'>
-                                <Link href="https://facebook.com"><Image src={'/images/footer/vec.svg'} alt="facebook" width={15} height={20} /></Link>
+                                <Link href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Renvestment on Facebook">
+                                    <Image src={'/images/footer/vec.svg'} alt="facebook" width={15} height={20} />
+                                </Link>
                             </div>
                             <div className='footer-icons'>
-                                <Link href="https://twitter.com"><Image src={'/images/footer/twitter.svg'} alt="twitter" width={20} height={20} /></Link>
+                                <Link href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Renvestment on Twitter">
+                                    <Image src={'/images/footer/twitter.svg'} alt="twitter" width={20} height={20} />
+                                </Link>
                             </div>
                             <div className='footer-icons'>
-                                <Link href="https://instagram.com"><Image src={'/images/footer/instagram.svg'} alt="instagram" width={20} height={20} /></Link>
+                                <Link href="https://www.instagram.com/renvestment/" target="_blank" rel="noopener noreferrer" aria-label="Renvestment on Instagram">
+                                    <Image src={'/images/footer/instagram.svg'} alt="instagram" width={20} height={20} />
+                                </Link>
                             </div>
                         </div>
                     </div>
